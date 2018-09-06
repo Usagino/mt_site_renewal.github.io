579703da6a3af94c76934fdb5399af10b95884de
Usagino <yuki.itoh@front-artgraph.com>
2018-09-06 14:12:42 +0900

    upload

--- script/script.js
+++ script/script.js
@@ -5,3 +5,26 @@ $(function(){
     $("li:nth-child("+i+") span").css('background','hsl(' + random_color + ', 62%, 73%)')
   }
 })
+
+let backt_and_title = [
+  "water","crayon","colorpencil"
+]
+let sections =[
+  '美しい色彩のグラデーション',
+  '温もりある手書きのタッチ',
+  '繊細あるニュアンスを楽しむ'
+]
+let i=0;
+Vue.component('content-com',{
+  template:"#content-component",
+  data:function(){
+    return{
+      back:backt_and_title[i],
+      title:backt_and_title[i],
+      section:sections[i++]
+    }
+  }
+})
+let app = new Vue({
+  el:"#contents"
+})
