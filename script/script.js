b1b3f7101fdcce1fb14532660d6443fa3a43d5ce
Usagino <yuki.itoh@front-artgraph.com>
2018-09-07 10:31:09 +0900

    update

--- script/script.js
+++ script/script.js
@@ -19,6 +19,7 @@ Vue.component('content-com',{
   template:"#content-component",
   data:function(){
     return{
+      id_num :i,
       back:backt_and_title[i],
       title:backt_and_title[i],
       tape:backt_and_title[i],
@@ -27,7 +28,7 @@ Vue.component('content-com',{
     }
   }
 })
-let app = new Vue({
+let contents = new Vue({
   el:"#contents"
 })
 
@@ -40,4 +41,43 @@ VueScrollTo.setDefaults({
     duration: 1000,
     easing: "ease-in-out",
 
+})
+
+let tape_num_list = [];
+
+for (let i = 0; i < 3; i++) {
+  let tape_num = document.getElementById( `section_image_${i}` ).getBoundingClientRect().top;
+  tape_num_list.push(tape_num);
+}
+console.log(tape_num_list);
+
+
+let app = new Vue({
+  el:'#contet',
+  data:{
+    scrollY: 0,
+    timer: null
+  },
+  created: function(){
+    //ハンドラを登録
+    window.addEventListener('scroll',this.handleScroll)
+  },
+  beforeDestroy: function(){
+    //ハンドラを解除(コンポーネントやSPAの場合忘れずに)
+    window.removeEventListener('scroll',this.handleScroll)
+  },
+  methods:{
+     // 違和感のない程度に200ms感覚でデータを更新する例
+    handleScroll: function(){
+      if(this.timer === null){
+        this.timer = setTimeout(function(){
+          this.scrollY = window.scrollY
+           //スクロールごとにコンソールに現在の高さを表示
+          console.log(this.scrollY)
+          clearTimeout(this.timer)
+          this.timer = null
+        }.bind(this), 200)
+      }
+    }
+  }
 })
